d4859067bb55429cf27338fab95309d63aff62ac
ameyi <114318190+ameyi@users.noreply.github.com>
2024-05-08 15:11:26 -0700

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,3 @@
+function calculator(){
+    window.location.replace("calculator.html");
+}
---
+++ hello.js
@@ -0,0 +1,12 @@
+function home(){
+    window.location.replace("frontend.html")
+}
+function alpha(){
+    window.location.replace("alpha.html")
+}
+function second(){
+    window.location.replace("second.html")
+}
+function regular(){
+    window.location.replace("calculator.html")
+}
